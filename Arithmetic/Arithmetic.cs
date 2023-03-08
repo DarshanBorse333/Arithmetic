b786cdaa2db18122f9fb4a4dc6d1ac0a8e67f683
DarshanBorse333 <Darshanborse@gmail.com>
2023-03-08 16:26:27 +0530

    Add project files.

---
+++ Arithmetic/Arithmetic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticDemo
+{
+    internal class Arithmetic
+    {
+
+        public int A { get; set; }
+
+        public int B { get; set; }
+
+
+        public void Add(int num1, int num2)  //parameters and arguments, accpeting multiple parameter
+        {
+            int result = num1 + num2;   
+            //Console.WriteLine("Addition of " +num1+ " and " +num2+ " is = " +result );
+            Console.WriteLine($"Addition of {num1} and {num2} is = {result}");
+        }
+
+        //function complex arguments or parameters 
+
+        public void Sub(Arithmetic arth)
+        {
+            int result = arth.A - arth.B;
+            Console.WriteLine($"Substraction of {arth.A} and {arth.B} is = {result}");
+        }
+
+        public void Mul(int num1, int num2, out int result)
+        {
+            result = num1 * num2;
+        }
+
+        public void Div(int num1 , int num2, out int result)
+        {
+            result = num1 / num2;
+        }
+    }
+}
---
+++ Arithmetic/Program.cs
@@ -0,0 +1,30 @@
+﻿using ArithmeticDemo;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Arithmetic arithmetic = new Arithmetic();
+        //arithmetic.Add(20,10);
+
+
+
+        Arithmetic arithmetic1 = new Arithmetic();
+        arithmetic1.A = 20;
+        arithmetic1.B = 10;
+
+        Arithmetic arithmetic2= new Arithmetic();
+        arithmetic2.Sub(arithmetic1);
+
+
+
+
+        int result;
+
+        arithmetic2.Mul(10, 20, out result);
+        Console.WriteLine($"Multiplication of 10 and 20 is {result}");
+
+        arithmetic2.Div(20, 5, out result);
+        Console.WriteLine($"Division of 20 and 5 is {result}");
+    } 
+}
